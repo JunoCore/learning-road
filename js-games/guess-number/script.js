11d4f47759c9e71fbec7b3eef45e210ead80fc7c
JunoCore <junoq69@gmail.com>
2025-07-08 19:43:36 +0400

    Update script.js

--- js-games/guess-number/script.js
+++ js-games/guess-number/script.js
@@ -9,7 +9,7 @@ import { getAuth, signInAnonymously }
   from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmMjvQrBRGcMn1xxNj4kiBEPAz37KT1w4",
+  apiKey: "AIzaSyA2gZiuyXas3t21kGGmm-a7WM9pzgrkZbc",
   authDomain: "guess-number-web.firebaseapp.com",
   projectId: "guess-number-web",
   storageBucket: "guess-number-web.firebasestorage.app",
